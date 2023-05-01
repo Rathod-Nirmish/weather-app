969b8d7980eb0bf4d6b5f379416f396a81484833
Nirmish <rathodnirmish0210@gmail.com>
2023-05-01 20:09:09 +0530

    weather app project

---
+++ src/Api.jsx
@@ -0,0 +1,14 @@
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "content-type": "application/octet-stream",
+    "X-RapidAPI-Key": "1517af4585mshf45e7d8c53307ddp1c24f9jsn1a675c1664ff",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "b8ae04b57fa8c9ab75689b94ba8ae4c4";
